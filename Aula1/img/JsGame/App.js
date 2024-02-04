77c37e6a3b2cc10545bf6cf031d3b1708ed4b569
vancorreia04 <109099486+vancorreia04@users.noreply.github.com>
2024-02-04 15:55:04 -0300

    Add files via upload

---
+++ Aula1/img/JsGame/App.js
@@ -0,0 +1,12 @@
+
+
+function exibirTextoNaTela(){
+let campo = document.querySelector(tag);
+campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function verificarChute(){
+console.log('O botão foi clicado');
+}
