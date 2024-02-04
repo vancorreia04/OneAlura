679fd06f4dd16fbfc94a43d59f47aa3b33057c5e
vancorreia04 <109099486+vancorreia04@users.noreply.github.com>
2024-02-04 15:29:25 -0300

    Add files via upload

---
+++ AulaDoisLogica/Listas/apps.js
@@ -0,0 +1,7 @@
+let frutas = ["Maçã", "Uva", "Laranja"];
+console.log(frutas[0]); // Saída: "Maçã"
+console.log(frutas[2]); // Saída: "Laranja"
+frutas.push("Morango");
+console.log(frutas); // Saída: ["Maçã", "Uva", "Laranja", "Morango"]
+frutas.pop();
+console.log(frutas); // Saída: ["Maçã", "Uva", "Laranja"]
